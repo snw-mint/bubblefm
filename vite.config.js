ff418559d8b495637b64e93fb6537e56dbfff911
Andry <andry.we@proton.me>
2026-07-06 17:56:11 -0300

    feat: add match leading

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,24 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        result: resolve(__dirname, 'result.html'),
-        match: resolve(__dirname, 'match/index.html'),
-        matchResult: resolve(__dirname, 'match/result.html')
-      }
-    }
+        main: resolve(__dirname, "index.html"),
+        result: resolve(__dirname, "result.html"),
+        match: resolve(__dirname, "match/index.html"),
+        matchResult: resolve(__dirname, "match/result.html"),
+      },
+    },
   },
   server: {
     proxy: {
-      '/api/deezer': {
-        target: 'https://api.deezer.com',
+      "/api/deezer": {
+        target: "https://api.deezer.com",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/deezer/, '')
-      }
-    }
-  }
+        rewrite: (path) => path.replace(/^\/api\/deezer/, ""),
+      },
+    },
+  },
 });
